1f02b705169c2029a17e1b56ef4a36963f709fa3
eawwal <eawwal@stevens.edu>
2017-12-03 22:43:41 -0500

    fml

--- app.js
+++ app.js
@@ -10,8 +10,11 @@ var serverPort = (process.env.PORT  || 4443);
 var https = require('https');
 var http = require('http');
 var server;
-server = https.createServer(options, app);
-
+if (process.env.LOCAL) {
+  server = https.createServer(options, app);
+} else {
+  server = http.createServer(app);
+}
 var io = require('socket.io')(server);
 
 var roomList = {};
@@ -22,6 +25,7 @@ app.get('/', function(req, res){
 });
 server.listen(serverPort, function(){
   console.log('server up and running at %s port', serverPort);
+  
 });
 
 function socketIdsInRoom(name) {
